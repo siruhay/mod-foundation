e06561ae112119c63b1896a623978075ec9065bf
monoland <mono.socialmedia@gmail.com>
2026-07-24 09:04:50 +0700

    feat: render dashboard KPI widgets

--- frontend/pages/dashboard/index.vue
+++ frontend/pages/dashboard/index.vue
@@ -1,6 +1,44 @@
 <template>
 	<page-home page-name="foundation-dashboard">
-		<template v-slot:default></template>
+		<template v-slot:default="{ record }">
+			<v-row dense>
+				<v-col md="3" cols="12">
+					<widget-number
+						color="teal"
+						icon="groups"
+						label="Total Komunitas (LKD)"
+						:value="`${record.totalCommunities ?? 0}`"
+					></widget-number>
+				</v-col>
+
+				<v-col md="3" cols="12">
+					<widget-number
+						color="blue"
+						icon="badge"
+						label="Total Pejabat/Petugas"
+						:value="`${record.totalOfficials ?? 0}`"
+					></widget-number>
+				</v-col>
+
+				<v-col md="3" cols="12">
+					<widget-number
+						color="indigo"
+						icon="person"
+						label="Total Anggota LKD"
+						:value="`${record.totalMembers ?? 0}`"
+					></widget-number>
+				</v-col>
+
+				<v-col md="3" cols="12">
+					<widget-number
+						color="deep-purple"
+						icon="account_tree"
+						label="Total Organisasi"
+						:value="`${record.totalOrganizations ?? 0}`"
+					></widget-number>
+				</v-col>
+			</v-row>
+		</template>
 	</page-home>
 </template>
 
